ffd4725918d78e7885316f6cc7cc9b78626df584
selukiler <jfc0007@alu.medac.es>
2024-12-02 23:39:13 +0100

    Add files via upload

---
+++ js/botones.js
@@ -0,0 +1,34 @@
+/*Mensaje de descripcion de producto */
+
+
+function mostrarMensaje1() {
+    alert("Camiseta Dragon Ball Z // Tallas: S, M, L, XL");
+}
+
+function mostrarMensaje2() {
+    alert("Pantalones rechulones // Tallas: S, M, L, XL, XXL");
+}
+
+function mostrarMensaje3() {
+    alert("Sudadera golfa // Tallas: L, XL, XXL, XXXL");
+}
+
+function mostrarMensaje4() {
+    alert("Gorro épico // Tallas: Cabezita, Cabeza, Cabezon, Sandia");
+}
+
+function rayo() {
+    alert("Para correr tan rapido y tan estiloso como él.");
+}
+
+function juan() {
+    alert("Camiseta que desprende flow y encima pareces hasta animalista");
+}
+
+function alonso() {
+    alert("Sin mas el diosito");
+}
+
+function hamster() {
+    alert("Es una camiseta con un hamster sin mas");
+}
